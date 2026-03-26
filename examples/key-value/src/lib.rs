19a5f36e32b0ce6cd308baf0277989443f2d65e2
Brian Hardock <brian.hardock@fermyon.com>
2026-03-25 20:59:16 -0600

    Factor the SDK
    Signed-off-by: Brian Hardock <brian.hardock@fermyon.com>

--- examples/key-value/src/lib.rs
+++ examples/key-value/src/lib.rs
@@ -1,44 +1,44 @@
-use http::{Method, StatusCode};
-use spin_sdk::{
-    http::{IntoResponse, Response},
-    http_component,
-    key_value::Store,
-};
+use spin_sdk::http::{IntoResponse, Request, EmptyBody, body::IncomingBodyExt};
+use spin_sdk::key_value::Store;
+use spin_sdk::http_service;
 
-#[http_component]
-fn handle_request(req: http::Request<Vec<u8>>) -> anyhow::Result<impl IntoResponse> {
+#[http_service]
+async fn handle_request(req: Request) -> anyhow::Result<impl IntoResponse> {
     // Open the default key-value store
-    let store = Store::open_default()?;
+    let store = Store::open_default().await?;
 
-    let (status, body) = match *req.method() {
-        Method::POST => {
+    match *req.method() {
+        http::Method::POST => {
             // Add the request (URI, body) tuple to the store
-            store.set(req.uri().path(), req.body().as_slice())?;
-            (StatusCode::OK, None)
+            let key = req.uri().path().to_string();
+            let bytes = req.into_body().bytes().await?;
+            store.set(key, bytes.to_vec()).await?;
+            Ok((http::StatusCode::OK, EmptyBody::new()))
         }
-        Method::GET => {
-            // Get the value associated with the request URI, or return a 404 if it's not present
-            match store.get(req.uri().path())? {
-                Some(value) => (StatusCode::OK, Some(value)),
-                None => (StatusCode::NOT_FOUND, None),
-            }
+        http::Method::GET => {
+            // // Get the value associated with the request URI, or return a 404 if it's not present
+            // match store.get(req.uri().path())? {
+            //     Some(value) => (StatusCode::OK, Some(value)),
+            //     None => (StatusCode::NOT_FOUND, None),
+            // }
+            todo!()
         }
-        Method::DELETE => {
+        http::Method::DELETE => {
             // Delete the value associated with the request URI, if present
-            store.delete(req.uri().path())?;
-            (StatusCode::OK, None)
+            store.delete(req.uri().path().to_string()).await?;
+            Ok((http::StatusCode::OK, EmptyBody::new()))
         }
-        Method::HEAD => {
+        http::Method::HEAD => {
             // Like GET, except do not return the value
-            let code = if store.exists(req.uri().path())? {
-                StatusCode::OK
+            let key= req.uri().path().to_string();
+            let code = if store.exists(key).await? {
+                http::StatusCode::OK
             } else {
-                StatusCode::NOT_FOUND
+                http::StatusCode::NOT_FOUND
             };
-            (code, None)
+            Ok((code, EmptyBody::new()))
         }
         // No other methods are currently supported
-        _ => (StatusCode::METHOD_NOT_ALLOWED, None),
-    };
-    Ok(Response::new(status, body))
+        _ => Ok((http::StatusCode::METHOD_NOT_ALLOWED, EmptyBody::new())),
+    }
 }
